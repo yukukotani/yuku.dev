bc0ffddf0cffffd167939ca80ef39c54c3516365
Yuku Kotani <yukukotani@gmail.com>
2020-09-13 14:49:57 +0900

    Introduce google analyrics

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,22 @@ import "../styles/globals.css";
 import Head from "next/head";
 import styles from "../styles/App.module.css";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className={styles.container}>
       <Head>
