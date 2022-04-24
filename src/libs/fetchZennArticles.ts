21fcf74f3b24a748faa95b3f2f49f16b96f3db69
Yuku Kotani <yukukotani@gmail.com>
2022-04-24 22:04:18 +0900

    show ubie org articles

--- src/libs/fetchZennArticles.ts
+++ src/libs/fetchZennArticles.ts
@@ -2,7 +2,7 @@ import { Article } from './../models/Article';
 import fetch from 'node-fetch';
 import { parseISO } from 'date-fns';
 
-export async function fetchZennArticles(): Promise<readonly Article[]> {
+export async function fetchYukuZennArticles(): Promise<readonly Article[]> {
   const res = await fetch('https://zenn.dev/api/articles?username=yuku&count=100&order=latest');
   const body = await res.json();
 
@@ -12,4 +12,20 @@ export async function fetchZennArticles(): Promise<readonly Article[]> {
     title: article.title,
     url: `https://zenn.dev/${article.user.username}/articles/${article.slug}`,
   }));
+}
+
+const ubieZennSlugs: string[] = ['firebase-auth-hack', 'oss-donation'];
+
+export async function fetchUbieZennArticles(): Promise<readonly Article[]> {
+  const res = await fetch('https://zenn.dev/api/articles?username=ubie&count=100&order=latest');
+  const body = await res.json();
+
+  return (body.articles as any[])
+    .filter((article) => ubieZennSlugs.includes(article.slug))
+    .map((article) => ({
+      source: 'zenn',
+      publishDate: parseISO(article.published_at),
+      title: article.title,
+      url: `https://zenn.dev/${article.user.username}/articles/${article.slug}`,
+    }));
 }
