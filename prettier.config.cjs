873204e0cca36482a171b39f1e8368ac436b9605
Yuku Kotani <yukukotani@gmail.com>
2022-12-17 01:58:44 +0900

    astro prettier plugin

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,1 +1,12 @@
-module.exports = require("@ubie/prettier-config");
+module.exports = {
+  ...require('@ubie/prettier-config'),
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
